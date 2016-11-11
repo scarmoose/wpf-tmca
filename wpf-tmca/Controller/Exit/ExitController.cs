9c7737b38844c9f6fa0ceda065c54cd3800f7c9a
Martin Roos <Martin Roos@DESKTOP-C2U8AO6>
2016-11-11 12:29:31 +0100

    Exit

---
+++ wpf-tmca/Controller/Exit/ExitController.cs
@@ -0,0 +1,22 @@
+﻿using GalaSoft.MvvmLight.Command;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_tmca.Controller.Exit
+{
+    class ExitController
+    {
+        private static readonly ExitController _self = new ExitController();
+        public static ExitController Instance => _self;
+
+        public RelayCommand ExitCommand => new RelayCommand(ExitProgram);
+
+        public void ExitProgram()
+        {
+            
+        }
+    }
+}
--- wpf-tmca/ViewModel/MainViewModel.cs
+++ wpf-tmca/ViewModel/MainViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
-using wpf_tmca.Controls.Exit;
+using wpf_tmca.Controller.Exit;
 
 namespace wpf_tmca.ViewModel
 {
@@ -14,6 +14,4 @@ namespace wpf_tmca.ViewModel
 
         public ICommand ExitCommand => exit.ExitCommand;
     }
-
-    
 }
