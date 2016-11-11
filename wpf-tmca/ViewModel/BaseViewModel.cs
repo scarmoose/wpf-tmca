482ff9dc0bca7ee6a612c34ed0b51be14bbfd9d9
scarmoose <thomas@liljegren.dk>
2016-11-11 12:30:28 +0100

    DragBehavior

--- wpf-tmca/ViewModel/BaseViewModel.cs
+++ wpf-tmca/ViewModel/BaseViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,5 +11,7 @@ namespace wpf_tmca.ViewModel
     class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) => PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
     }
 }
