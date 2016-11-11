c4504c60ec2e28ef46391fb253ae55ec342451b1
Martin Roos <Martin Roos@DESKTOP-C2U8AO6>
2016-11-11 12:21:24 +0100

    ExitCommand

---
+++ wpf-tmca/ViewModel/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using wpf_tmca.Controls.Exit;
+
+namespace wpf_tmca.ViewModel
+{
+    class MainViewModel
+    {
+        private ExitController exit => ExitController.Instance;
+
+        public ICommand ExitCommand => exit.ExitCommand;
+    }
+
+    
+}
