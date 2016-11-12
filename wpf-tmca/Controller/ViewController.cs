3689f6f97db0586209772c91ea9346c91c0e3217
Christoffer <s145089@student.dtu.dk>
2016-11-12 16:06:58 +0100

    ViewController

--- wpf-tmca/Controller/ViewController.cs
+++ wpf-tmca/Controller/ViewController.cs
@@ -10,26 +10,81 @@ namespace wpf_tmca.Controller
 {
     class ViewController
     {
+        private bool _statusBar, _toolBox;
+
         private static readonly ViewController _self = new ViewController();
         public static ViewController Instance => _self;
 
         public RelayCommand HideStatusBarCommand => new RelayCommand(HideStatusBar);
 
+        public RelayCommand HideToolBoxCommand => new RelayCommand(HideToolBox);
+
+        private ViewController()
+        {
+            _statusBar = true;
+            _toolBox = true;
+        }
+
+        public bool StatusBar
+        {
+            get
+            {
+                return _statusBar;
+            }
+            set
+            {
+                _statusBar = value;
+            }
+        }
+
+        public bool ToolBox
+        {
+            get
+            {
+                return _toolBox;
+            }
+            set
+            {
+                _toolBox = value;
+            }
+        }
+
         public void HideStatusBar()
         {
-            var mainWindow = (Application.Current.MainWindow as MainWindow);
-            if (mainWindow != null)
+            if(StatusBar)
             {
-                HideStatusBar();
+                StatusBar = false;
             }
+            else
+            {
+                StatusBar = true;
+            }
+            Console.WriteLine(StatusBar);
         }
 
         public void HideToolBox()
         {
-            var mainWindow = (Application.Current.MainWindow as MainWindow);
-            if (mainWindow != null)
+            if (ToolBox)
             {
-                //Hide ToolBox
+                ToolBox = false;
+            }
+            else
+            {
+                ToolBox = true;
+            }
+            Console.WriteLine(ToolBox);
+
+        }
+
+        public string getVisibility(bool visible)
+        {
+            if (visible)
+            {
+                return "Visible";
+            }
+            else
+            {
+                return "Hidden";
             }
         }
     }
--- wpf-tmca/ViewModel/MainViewModel.cs
+++ wpf-tmca/ViewModel/MainViewModel.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using wpf_tmca.Controller;
 using wpf_tmca.Controller.Exit;
 
 namespace wpf_tmca.ViewModel
@@ -14,7 +16,83 @@ namespace wpf_tmca.ViewModel
 
         #region Commands
         public ICommand ExitCommand => exit.ExitCommand;
+        public ICommand HideStatusBarCommand => new RelayCommand(HideStatusBar);
+        public ICommand HideToolBoxCommand => new RelayCommand(HideToolBox);
 
         #endregion
+
+        #region View
+
+        private bool _statusBar, _toolBox;
+        private string _statusBarVisability, _toolBoxVisability;
+
+        public void HideStatusBar()
+        {
+            if (_statusBar)
+            {
+                _statusBar = false;
+            }
+            else
+            {
+                _statusBar = true;
+            }
+
+            StatusBarVisability = getVisibility(_statusBar);
+        }
+
+        public void HideToolBox()
+        {
+            if (_toolBox)
+            {
+                _toolBox = false;
+            }
+            else
+            {
+                _toolBox = true;
+            }
+
+            ToolBoxVisability = getVisibility(_toolBox);
+        }
+
+        public string getVisibility(bool visible)
+        {
+            if (visible)
+            {
+                return "Visible";
+            }
+            else
+            {
+                return "Hidden";
+            }
+        }
+
+        public string StatusBarVisability
+        {
+            get { return _statusBarVisability; }
+            set
+            {
+                _statusBarVisability = value;
+                this.OnPropertyChanged("StatusBarVisability");
+            }
+        }
+
+        public string ToolBoxVisability
+        {
+            get { return _toolBoxVisability; }
+            set
+            {
+                _toolBoxVisability = value;
+                this.OnPropertyChanged("ToolBoxVisability");
+            }
+        }
+
+        #endregion
+
+        public MainViewModel()
+        {
+            _statusBar = true;
+            _toolBox = true;
+        }
+
     }
 }
