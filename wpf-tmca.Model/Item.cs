087fc2cd6542462aaec24f51734fa75037b00b78
Christoffer <s145089@student.dtu.dk>
2016-11-12 13:46:27 +0100

    Public

---
+++ wpf-tmca.Model/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_tmca.Model
+{
+    class Item : IItem
+    {
+        private static int _itemNumber;
+
+        public List<string> Context { get; set; }
+        public double Height { get; set; }
+        public int ItemNumber { get; set; } = _itemNumber;
+        public EItem Type { get; set; }
+        public double Weight { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+        public string toString() => 
+    }
+}
---
+++ wpf-tmca.Model/Line.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_tmca.Model
+{
+    class Line : ILine
+    {
+        public int fromPoint { get; set; }
+        public string Label { get; set; }
+        public int toPoint { get; set; }
+        public ELine Type { get; set; }
+    }
+}
