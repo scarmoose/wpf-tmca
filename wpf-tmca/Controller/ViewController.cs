13c5dd38c9e1e73c240431e94d850f33cff9ad4b
Christoffer <s145089@student.dtu.dk>
2016-11-12 15:03:49 +0100

    HideStatusBar

--- wpf-tmca/Controller/ViewController.cs
+++ wpf-tmca/Controller/ViewController.cs
@@ -1,16 +1,36 @@
-﻿using System;
+﻿using GalaSoft.MvvmLight.Command;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace wpf_tmca.Controller
 {
     class ViewController
     {
+        private static readonly ViewController _self = new ViewController();
+        public static ViewController Instance => _self;
+
+        public RelayCommand HideStatusBarCommand => new RelayCommand(HideStatusBar);
+
         public void HideStatusBar()
         {
+            var mainWindow = (Application.Current.MainWindow as MainWindow);
+            if (mainWindow != null)
+            {
+                HideStatusBar();
+            }
+        }
 
+        public void HideToolBox()
+        {
+            var mainWindow = (Application.Current.MainWindow as MainWindow);
+            if (mainWindow != null)
+            {
+                //Hide ToolBox
+            }
         }
     }
 }
