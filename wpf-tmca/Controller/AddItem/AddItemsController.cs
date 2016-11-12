49b85cece28048d9c7fd87a7164030d52b8c0278
Martin Roos <Martin Roos@DESKTOP-PJ1BKLU>
2016-11-12 10:59:24 +0100

    Toolbox + additems beginning

---
+++ wpf-tmca/Controller/AddItem/AddItemsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_tmca.Controller.Add
+{
+    class AddItemsController
+    {
+        private static readonly AddItemsController _self = new AddItemsController();
+        public static AddItemsController Instance => _self;
+
+
+    }
+}
