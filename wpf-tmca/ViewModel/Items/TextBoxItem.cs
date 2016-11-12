4ca9366e47cfff4257fb77c064aea8cf623dbb60
Martin Roos <Martin Roos@DESKTOP-PJ1BKLU>
2016-11-12 14:45:46 +0100

    item

---
+++ wpf-tmca/ViewModel/Items/TextBoxItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_tmca.Model;
+
+namespace wpf_tmca.ViewModel.Items
+{
+    class TextBoxItem : ItemViewModel
+    {
+        public TextBoxItem(Item item) : base(item)
+        {
+            Width = 60;
+            Height = 23;
+        }
+
+        public TextBoxItem():this(new Item() { Type=EItem.TextBox})
+        {
+
+        }
+        
+    }
+}
