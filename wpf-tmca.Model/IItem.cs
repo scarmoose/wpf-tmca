6b04ad3a4d1c9b4db05c9e933930380eed9798c5
Christoffer <s145089@student.dtu.dk>
2016-11-12 11:18:02 +0100

    Item

---
+++ wpf-tmca.Model/IItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_tmca.Model
+{
+    interface IItem
+    {
+        List<string> Context { get; set; }
+        double Height { get; }
+        double Weight { get; }
+        double X { get; }
+        double Y { get; }
+        int ItemNumber { get; }
+        EItem Type { get; }
+
+        string toString();
+    }
+}
