de544247a6026a99bfc11f4770140d714a802ab6
Martin Roos <Martin Roos@DESKTOP-PJ1BKLU>
2016-11-12 13:46:55 +0100

    Public

---
+++ wpf-tmca/ViewModel/ItemViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using wpf_tmca.Model;
+
+namespace wpf_tmca.ViewModel
+{
+    class ItemViewModel : BaseViewModel, IItem
+    {
+        private bool _isSelected;
+        private Point _initialMousePostion;
+        private bool _isMoving;
+        private bool _isConnectingShapes;
+        private Point _initialShapePostion;
+
+
+    }
+}
