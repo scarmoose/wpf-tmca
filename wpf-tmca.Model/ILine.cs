4f97ada4f0737b3ca6b94afba6a3dda8d50ecc1d
Christoffer <s145089@student.dtu.dk>
2016-11-12 11:03:37 +0100

    wpf.model

---
+++ wpf-tmca.Model/ILine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_tmca.Model
+{
+    interface ILine
+    {
+        int fromPoint { get; }
+        int toPoint { get; }
+        string Label { get; }
+        ELine Type { get; }
+    }
+}
